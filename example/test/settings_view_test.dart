b4e7306b524fe142d69a92e1952dcee0f7adc945
Daniil <bondarev@surf.dev>
2024-01-23 15:09:54 +0300

    feat: Prepare package for release

--- example/test/settings_view_test.dart
+++ example/test/settings_view_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:provider/provider.dart';
 import 'package:surf_widget_test_composer/surf_widget_test_composer.dart';
@@ -9,11 +11,44 @@ void main() {
 
   /// Generate golden.
   testWidget<SettingsView>(
-    desc: 'SettingsScreen',
+    desc: 'SettingsScreen - goldens',
     widgetBuilder: (context, _) => widget.build(context),
     setup: (context, data) {
       when(() => context.watch<SettingsController>().themeMode)
           .thenReturn(data);
     },
   );
+
+  /// Widget test for DropdownButton.
+  testWidget<SettingsView>(
+      desc: 'SettingsScreen - DropdownButton test',
+      widgetBuilder: (context, _) => widget.build(context),
+      setup: (context, data) {
+        when(() => context.watch<SettingsController>().themeMode)
+            .thenReturn(data);
+        when(() => context
+                .read<SettingsController>()
+                .updateThemeMode(ThemeMode.dark))
+            .thenAnswer((invocation) => Future.value());
+      },
+      test: (tester, _) async {
+        /// A way to get context inside test.
+        Builder(builder: (context) {
+          final updateThemeDropdownButton = find.byType(
+            DropdownButton,
+          );
+
+          /// Check if there is only one DropdownButton.
+          expect(updateThemeDropdownButton, findsOneWidget);
+
+          /// Check if updateThemeMode is called when the DropdownButton item is tapped.
+          tester.tap(updateThemeDropdownButton).then(
+                (value) => verify(
+                  () => context.read<SettingsController>().updateThemeMode,
+                ),
+              );
+
+          return const Placeholder();
+        });
+      });
 }
