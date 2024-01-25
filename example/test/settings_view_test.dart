a496d8c4c542bfb430bba86e86ca2668ec98d2a7
Daniil <bondarev@surf.dev>
2024-01-26 01:50:44 +0300

    fix: add mock controller

--- example/test/settings_view_test.dart
+++ example/test/settings_view_test.dart
@@ -1,54 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:provider/provider.dart';
 import 'package:surf_widget_test_composer/surf_widget_test_composer.dart';
 import 'package:surf_widget_test_composer_example/src/settings/settings_controller.dart';
 import 'package:surf_widget_test_composer_example/src/settings/settings_view.dart';
 
+class MockSettingsController extends Mock implements SettingsController {}
+
 void main() {
-  const widget = SettingsView();
+  final mockSettingsController = MockSettingsController();
+
+  final widget = SettingsView(
+    settingsController: mockSettingsController,
+  );
 
   /// Generate golden.
   testWidget<SettingsView>(
-    desc: 'SettingsScreen - goldens',
+    desc: 'SettingsScreen',
     widgetBuilder: (context, _) => widget.build(context),
     setup: (context, data) {
-      when(() => context.watch<SettingsController>().themeMode)
-          .thenReturn(data);
+      when(() => mockSettingsController.themeMode).thenReturn(ThemeMode.dark);
+      when(() => mockSettingsController.updateThemeMode(any()))
+          .thenAnswer((invocation) => Future.value());
+    },
+
+    /// Widget tests
+    test: (tester, context) async {
+      final button = find.byType(DropdownButton<ThemeMode>);
+
+      expect(button, findsOneWidget);
+
+      final floatingActionButton = find.byIcon(Icons.mode_night);
+
+      expect(floatingActionButton, findsOneWidget);
+
+      verifyNever(() => mockSettingsController.updateThemeMode(any()));
+
+      await tester.tap(floatingActionButton);
+
+      verify(() => mockSettingsController.updateThemeMode(any()));
     },
   );
-
-  /// Widget test for DropdownButton.
-  testWidget<SettingsView>(
-      desc: 'SettingsScreen - DropdownButton test',
-      widgetBuilder: (context, _) => widget.build(context),
-      setup: (context, data) {
-        when(() => context.watch<SettingsController>().themeMode)
-            .thenReturn(data);
-        when(() => context
-                .read<SettingsController>()
-                .updateThemeMode(ThemeMode.dark))
-            .thenAnswer((invocation) => Future.value());
-      },
-      test: (tester, _) async {
-        /// A way to get context inside test.
-        Builder(builder: (context) {
-          final updateThemeDropdownButton = find.byType(
-            DropdownButton,
-          );
-
-          /// Check if there is only one DropdownButton.
-          expect(updateThemeDropdownButton, findsOneWidget);
-
-          /// Check if updateThemeMode is called when the DropdownButton item is tapped.
-          tester.tap(updateThemeDropdownButton).then(
-                (value) => verify(
-                  () => context.read<SettingsController>().updateThemeMode,
-                ),
-              );
-
-          return const Placeholder();
-        });
-      });
 }
